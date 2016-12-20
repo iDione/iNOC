27b5a141b6158a5c59b100b745d408ceaac1ad85
Adarsh Devanhalli <adarshadarsh@gmail.com>
2016-12-20 11:05:46 -0600

    first commit, includes the first migration

---
+++ src/main/java/com/idione/inoc/InocApplication.java
@@ -0,0 +1,12 @@
+package com.idione.inoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InocApplication.class, args);
+	}
+}
---
+++ src/migrations/20161218095000_create_initial_tables.sql
@@ -0,0 +1,83 @@
+CREATE TABLE clients (
+  id           SERIAL PRIMARY KEY,
+  name         VARCHAR(128),
+  email        VARCHAR(128),
+  password     VARCHAR(256),
+  created_at   TIMESTAMP,
+  updated_at   TIMESTAMP,
+  deleted      INT DEFAULT 0
+);
+
+CREATE TABLE poc_users (
+  id             SERIAL PRIMARY KEY,
+  client_id      INT NOT NULL,
+  first_name     VARCHAR(128),
+  last_name      VARCHAR(128),
+  role           VARCHAR(128),
+  email_address  VARCHAR(128),
+  phone_number   VARCHAR(128),
+  created_at     TIMESTAMP,
+  updated_at     TIMESTAMP,
+  deleted        INT DEFAULT 0
+);
+
+CREATE TABLE mailing_groups (
+  id             SERIAL PRIMARY KEY,
+  client_id      INT NOT NULL,
+  name           VARCHAR(128),
+  created_at     TIMESTAMP,
+  updated_at     TIMESTAMP,
+  deleted        INT DEFAULT 0
+);
+
+CREATE TABLE mailing_group_poc_users (
+  id                SERIAL PRIMARY KEY,
+  mailing_group_id  INT NOT NULL,
+  poc_user_id       INT NOT NULL,
+  created_at        TIMESTAMP
+);
+
+CREATE TABLE filters (
+  id                SERIAL PRIMARY KEY,
+  name              VARCHAR(128),
+  time_interval     INT NOT NULL,
+  retries           INT NOT NULL,
+  mailing_group_id  INT NOT NULL,
+  created_at        TIMESTAMP,
+  updated_at        TIMESTAMP,
+  deleted           INT DEFAULT 0
+);
+
+CREATE TABLE filter_keywords (
+  id             SERIAL PRIMARY KEY,
+  filter_id      INT NOT NULL,
+  keyword        VARCHAR(128),
+  created_at     TIMESTAMP,
+  updated_at     TIMESTAMP
+);
+
+CREATE TABLE filter_poc_users (
+  id             SERIAL PRIMARY KEY,
+  filter_id      INT NOT NULL,
+  poc_user_id    INT NOT NULL,
+  created_at     TIMESTAMP,
+  updated_at     TIMESTAMP,
+  deleted        INT DEFAULT 0
+);
+
+CREATE TABLE issues (
+  id             SERIAL PRIMARY KEY,
+  email_id       VARCHAR(128),
+  filter_id      INT,
+  created_at     TIMESTAMP
+);
+
+CREATE TABLE issue_pos_user (
+  id             SERIAL PRIMARY KEY,
+  issue_id       INT NOT NULL,
+  poc_user_id    INT NOT NULL,
+  user_response  INT,
+  accepted       INT DEFAULT 0,
+  created_at     TIMESTAMP,
+  updated_at     TIMESTAMP
+);
