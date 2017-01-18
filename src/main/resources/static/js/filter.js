8f13c95fbc98f4f30eb05ca658f0983ccfcd2de4
Adarsh Devanhalli <adarshadarsh@gmail.com>
2017-01-18 00:26:13 -0600

    added dropdown to select poc users for filters and order them

--- src/main/resources/static/js/filter.js
+++ src/main/resources/static/js/filter.js
@@ -1,4 +1,18 @@
 $(document).ready(function () {
+    function moveUsers(origin, dest) {
+        $(origin).find(':selected').appendTo(dest);
+    };
+
+    $('#addUser').click(function(e) {
+    	e.preventDefault();
+    	moveUsers('#allPocUserIds', '#pocUserIds');
+    });
+
+    $('#removeUser').click(function(e) {
+    	e.preventDefault();
+    	moveUsers('#pocUserIds', '#allPocUserIds');
+    });
+
     $("#addKeyword").click(function (e) {
     	e.preventDefault();
         if ($('#keyword').val() != "") {
@@ -9,17 +23,21 @@ $(document).ready(function () {
             $('#keyword').focus();
          }
     });
-    
+
     $("#removeKeyword").click(function (e) {
     	e.preventDefault();
     	$("#keywords option:selected").remove();
     });
-    
+
     $("#updateFilter").click(function (e) {
     	$('#keywords option').prop('selected', true);
+    	$('#pocUserIds option').prop('selected', true);
     });
-    
+
     $("#createFilter").click(function (e) {
     	$('#keywords option').prop('selected', true);
+    	$('#pocUserIds option').prop('selected', true);
     });
+
+    $("#addUser").click();
 });
